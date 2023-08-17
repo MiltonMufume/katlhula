aa85824ce4550208e9d5a127c0e0397404c33ef4
milton-mufume <fernando.mufume@csaude.org.mz>
2023-08-17 10:21:50 +0200

    Creating jpa branch to database connection

---
+++ src/main/java/co/mz/gposoft/katlhula/KatlhulaApplication.java
@@ -0,0 +1,13 @@
+package co.mz.gposoft.katlhula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatlhulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatlhulaApplication.class, args);
+	}
+
+}
