cc5b60295f6e152e695f746f8d5e3d01d05fd9fb
miltonmufume <miltonmufume@gmail.com>
2023-08-20 17:25:18 +0200

    Adding annotation to the entities

--- src/main/java/co/mz/gposoft/katlhula/domain/ArticleForSale.java
+++ src/main/java/co/mz/gposoft/katlhula/domain/ArticleForSale.java
@@ -1,18 +1,30 @@
 package co.mz.gposoft.katlhula.domain;
 
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
+@Table(name = "article_for_sale")
 public class ArticleForSale {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(name = "product_to_sell_id", nullable = false)
     private List<ProductToSell> products;
 
+    @Column(name = "user_id", nullable = false)
     private User createdBy;
 
     public ArticleForSale(List<ProductToSell> products, User createdBy) {
         this.products = products;
         this.createdBy = createdBy;
+    }
+
+    public ArticleForSale() {
+
     }
 
     public long getId() {
