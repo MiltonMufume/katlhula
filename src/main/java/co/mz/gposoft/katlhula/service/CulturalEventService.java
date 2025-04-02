3b70df98c851f0c218a996f4587776a719af0349
miltonmufume <miltonmufume@gmail.com>
2025-04-02 10:49:14 +0200

    Removing springfox to add springdoc a more moddern spring API to document APIs using swagger/OenAPI recomendations

--- src/main/java/co/mz/gposoft/katlhula/service/CulturalEventService.java
+++ src/main/java/co/mz/gposoft/katlhula/service/CulturalEventService.java
@@ -2,7 +2,7 @@ package co.mz.gposoft.katlhula.service;
 
 import co.mz.gposoft.katlhula.dao.CulturalEventRepository;
 import co.mz.gposoft.katlhula.domain.CulturalEvent;
-import io.swagger.annotations.Api;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
